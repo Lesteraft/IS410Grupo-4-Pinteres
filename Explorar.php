8988a7bfb43b900608b7d212bef59ad800f30dec
YozabethGarcia <31870247+YozabethGarcia@users.noreply.github.com>
2018-08-20 20:47:23 -0600

    blablabla

--- Explorar.php
+++ Explorar.php
@@ -12,71 +12,131 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="img/login.jpg">
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="img/login/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/estilos-usuario.css">
+    <link rel="stylesheet" href="fuentes/css/all.css">
     <link href="css/custom.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 </head>
 <body style="display:block">
-    <nav class="navbar navbar-expand-lg navbar-expand-md navbar-light bg-light fixed-top" style="height: 64px;">
+<header id="header-pint">
+            <div class="navbar">
 
-        <div>
-            <button type="button" class="btn btn-light letraNav rounded-circle">
-                <img src="img/login.jpg" width="29" height="29">
-            </button>
-        </div>
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-    
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-    
-            <div class="input-group mb-3" height="40" id="input-navbar">
-                <div style="padding: 8px 0px; margin-right:-50px;" class="input-group-prepend ">
-                    <i class="fas fa-search faz"></i>
+                <div class="ocultar">
+                    <img src="img/login/favicon.png">
                 </div>
-                <input type="text" class="form-control" placeholder="Buscar" id="txt-buscar" aria-label="Username" aria-describedby="basic-addon1">
-            </div>
-            
-            <div>
-                <button type="button" class="btn btn-light letraNav " style="border-radius: 25px; height: 43px;"
-                >Inicio</button>
-            </div>
+    
+                <div style="flex: 1 1 auto;" class="a12 ocultar">
+                    <div class="input-group mb-3 ocultar" id="input-navbar">
+                        <div style="padding: 5px 0px; margin-right: -26px;">
+                            <i class="fas fa-search faz"></i>
+                        </div>
+                        <input type="text" class="form-control" placeholder="Buscar" id="txt-buscar" aria-label="Username" aria-describedby="basic-addon1">
+                    </div>            
+                </div>
+                
+                <div class="navbar" id="botones-navbar">
 
-            <div>
-                <button type="button" class="btn btn-light letraNav" style="border-radius: 25px; height: 43px;">
-                    Explorar</button>
+                    <div class="mostrar">
+                        <img src="img/login/favicon.png">
+                    </div>
+
+                    <div class="mostrar cir-tras-hov" style="text-align: center; flex: none; margin-left: 10px;">
+                        <i class="fas fa-search faz"></i>
+                    </div> 
+
+                    <a href="Inicio.php">
+                    <div class="cir-tras-hov ocultar">
+                        <div>
+                            Inicio
+                        </div>
+                    </div>
+                    </a>                                    
+
+                    <a href="Siguiendo.html">
+                    <div class="cir-tras-hov">
+                            <div class="ocultar">
+                                Siguiendo
+                            </div>
+                            <div class="mostrar">
+                                <i class="fas fa-user-friends faz"></i>
+                            </div>
+                    </div>
+                    </a>
+
+                    <a href="Explorar.php">
+                    <div class="cir-tras-hov">
+                        <div class="ocultar">
+                            Explorar
+                        </div>
+                        <div class="mostrar">
+                            <i class="fas fa-compass faz"></i>
+                        </div>
+                    </div>
+                    </a>
+
+                    <div class="cir-tras-hov aa a13" >
+                        <div class="ocultar">
+                            <img class="rounded-circle img-thumbnail" src="img/Usuario/2.jpg" style="width: 30px;"> Lester
+                        </div>
+                        <div class="mostrar">
+                            <img class="rounded-circle img-thumbnail" src="img/Usuario/2.jpg" style="width: 30px;">
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="dropleftdown cir-tras-hov aa a13" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <div class="" id="dropdownMenuMensajes"  >     
+                                <i class="fas fa-comment-dots faz"></i>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuMensajes">
+                                    <div class="tcdd" style="right: 33%;"><svg width="24" height="24"><path d="M0 24 L12 12 L24 24"></path></svg></div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="dropleftdown cir-tras-hov aa a13" data-toggle="dropdown"  aria-haspopup="true" aria-expanded="false">
+                            <div class="" id="dropdownMenuNotificaciones" >
+                                    <i class="fas fa-bell faz"></i>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuNotificaciones">
+                                    <div class="tcdd" style="right: 18%;"><svg width="24" height="24"><path d="M0 24 L12 12 L24 24"></path></svg></div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="dropleftdown cir-tras-hov aa a13" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <div class="" id="dropdownMenuOpciones"  >
+                                <i class="fas fa-ellipsis-h faz"></i>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuOpciones">
+                                    <div class="tcdd"><svg width="24" height="24"><path d="M0 24 L12 12 L24 24"></path></svg></div>
+                                    <div class="dropdown-item-text bdbxsi"><h4>Consigue una cuenta para empresas gratuita</h4></div>
+                                    <div class="dropdown-item-text bdbxsi">Desbloquea herramientas profesionales como analytics y anuncios</div>
+                                    <div class="bdbxsi"><button type="button" id="btn-actualizar-ahora" >Actualizar ahora</button></div>
+                                    <div class="dropdown-item bdbxsi"></div>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Editar Ajustes</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Obtener ayuda</a>
+                                    <a class="dropdown-item" href="#">Ver términos y privacidad</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Cerrar sesión</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
-
-            <div>
-                <button type="button" class="btn btn-light letraNav" style="border-radius: 50px">
-                        <img src="img/perfil.jpg" width="30" height="30" style="border-radius: 100px;">
-                        aczayozabeth
-                </button>
-            </div>
-
-            <div>
-                <button type="button" class="btn btn-light letraNav rounded-circle">
-                    <i class="fas fa-comment-dots"></i>
-                </button>
-            </div>
-
-            <div>
-                <button type="button" class="btn btn-light letraNav rounded-circle">
-                    <i class="fas fa-bell"></i>
-                </button>
-            </div>
-
-            <div>
-                <button type="button" class="btn btn-light letraNav rounded-circle">
-                    <i class="fas fa-ellipsis-h"></i>
-                </button>
-            </div> 
-        </div> 
-    </nav>
-
-    <main class="container " style=" margin: 5px; padding: 65px 0px 0px 0px; max-width: 100%;">
-        <hr>
-        <div >
+            <hr>
+    </header>
+    <main class="container " style=" margin: 5px; padding: 0px 0px 0px 0px; max-width: 100%;">
+        <div>
 
             <div style="padding-left: 300px;">
                 <h3 style="color: #333; font-size: 48px; margin-top: 50px; " class="letraNav">Explorar</h3>
@@ -96,7 +156,7 @@
 
         </div>
 
-        <div class="row" style=" margin: 0px; padding: 20px 0px 0px 0px; max-width: 100%;">
+        <div class="row columna" style=" margin: 0px; padding: 20px 0px 0px 0px; max-width: 100%;  display:block">
 
             <?php
               //Forma facil
@@ -105,7 +165,7 @@
               while(($linea = fgets($archivo))){
                 $partes = explode(",",$linea);
 
-                echo'<div class="card columna " style="max-width: 260px; padding:8px;" id="'.$partes[0].'">
+                echo'<div class="card " style="max-width: 260px; padding:8px;" id="'.$partes[0].'">
                         <img class="card-img-top" src="img/'.$partes[1].'" id="idImagen" >
                         <div class="card-body row">
                             <h4 class="card-text letraNav">'.$partes[2].'</h4>
@@ -121,6 +181,8 @@
     </main>
     <script src="js/jquery.min.js"></script>
     <script src="js/controlador.js"></script>
+    <script src="jquery/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
     <script>
 
         var contador=0;
--- Inicio.php
+++ Inicio.php
@@ -1,111 +1,171 @@
-<?php
-  //Esta linea verifica que si no hay un parametro en GET con la carpeta actual lo redireccione a inicio:
-  if(!isset($_GET['carpeta']))
-    header("location: inicio.php?carpeta=inicio"); //Redireccionar a otra pagina*/
-?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" >
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Pinterest</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="img/login.jpg">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/custom.css" rel="stylesheet">
+    <link rel="shortcut icon" href="img/login/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/estilos-usuario.css">
+    <link rel="stylesheet" href="fuentes/css/all.css">
+    <link href="custom.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 
 </head>
-<body>
+<body >
+<div>
+<header id="header-pint">
+            <div class="navbar">
 
-    <nav class="navbar navbar-expand-lg navbar-expand-md navbar-light bg-light fixed-top" style="height: 64px;">
+                <div class="ocultar">
+                    <img src="img/login/favicon.png">
+                </div>
+    
+                <div style="flex: 1 1 auto;" class="a12 ocultar">
+                    <div class="input-group mb-3 ocultar" id="input-navbar">
+                        <div style="padding: 5px 0px; margin-right: -26px;">
+                            <i class="fas fa-search faz"></i>
+                        </div>
+                        <input type="text" class="form-control" placeholder="Buscar" id="txt-buscar" aria-label="Username" aria-describedby="basic-addon1">
+                    </div>            
+                </div>
+                
+                <div class="navbar" id="botones-navbar">
 
-        <div>
-            <button type="button" class="btn btn-light letraNav rounded-circle">
-                    <img src="img/login.jpg" width="29" height="29">
+                    <div class="mostrar">
+                        <img src="img/login/favicon.png">
+                    </div>
+
+                    <div class="mostrar cir-tras-hov" style="text-align: center; flex: none; margin-left: 10px;">
+                        <i class="fas fa-search faz"></i>
+                    </div> 
+
+                    <a href="Inicio.php">
+                    <div class="cir-tras-hov ocultar">
+                        <div>
+                            Inicio
+                        </div>
+                    </div>
+                    </a>                                    
+
+                    <a href="Siguiendo.html">
+                    <div class="cir-tras-hov">
+                            <div class="ocultar">
+                                Siguiendo
+                            </div>
+                            <div class="mostrar">
+                                <i class="fas fa-user-friends faz"></i>
+                            </div>
+                    </div>
+                    </a>
+
+                    <a href="Explorar.php">
+                    <div class="cir-tras-hov">
+                        <div class="ocultar">
+                            Explorar
+                        </div>
+                        <div class="mostrar">
+                            <i class="fas fa-compass faz"></i>
+                        </div>
+                    </div>
+                    </a>
+
+                    <div class="cir-tras-hov aa a13" >
+                        <div class="ocultar">
+                            <img class="rounded-circle img-thumbnail" src="img/Usuario/2.jpg" style="width: 30px;"> Lester
+                        </div>
+                        <div class="mostrar">
+                            <img class="rounded-circle img-thumbnail" src="img/Usuario/2.jpg" style="width: 30px;">
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="dropleftdown cir-tras-hov aa a13" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <div class="" id="dropdownMenuMensajes"  >     
+                                <i class="fas fa-comment-dots faz"></i>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuMensajes">
+                                    <div class="tcdd" style="right: 33%;"><svg width="24" height="24"><path d="M0 24 L12 12 L24 24"></path></svg></div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="dropleftdown cir-tras-hov aa a13" data-toggle="dropdown"  aria-haspopup="true" aria-expanded="false">
+                            <div class="" id="dropdownMenuNotificaciones" >
+                                    <i class="fas fa-bell faz"></i>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuNotificaciones">
+                                    <div class="tcdd" style="right: 18%;"><svg width="24" height="24"><path d="M0 24 L12 12 L24 24"></path></svg></div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="dropleftdown cir-tras-hov aa a13" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <div class="" id="dropdownMenuOpciones"  >
+                                <i class="fas fa-ellipsis-h faz"></i>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuOpciones">
+                                    <div class="tcdd"><svg width="24" height="24"><path d="M0 24 L12 12 L24 24"></path></svg></div>
+                                    <div class="dropdown-item-text bdbxsi"><h4>Consigue una cuenta para empresas gratuita</h4></div>
+                                    <div class="dropdown-item-text bdbxsi">Desbloquea herramientas profesionales como analytics y anuncios</div>
+                                    <div class="bdbxsi"><button type="button" id="btn-actualizar-ahora" >Actualizar ahora</button></div>
+                                    <div class="dropdown-item bdbxsi"></div>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Editar Ajustes</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Obtener ayuda</a>
+                                    <a class="dropdown-item" href="#">Ver términos y privacidad</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Cerrar sesión</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+            <hr>
+    </header>
+</div>
+
+<div class="columna" style="margin:3px;padding-top:24px;" id="CuerpoImg">
+    
+</div>
+
+<button data-toggle="modal" data-target="#modal-Pin" type="button" class="btn btn-agregar rounded-circle">
+    <i class="fas fa-plus"></i>
+</button>
+
+<!-- Crear Pin -->
+<div class="modal fade" id="modal-Pin" tabindex="-1" role="dialog"  aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title">Crear Pin</h5>
+            <button type="button" class="close btn-light rounded-circle" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
             </button>
         </div>
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-
-            <div class="input-group mb-3" height="40" id="input-navbar">
-                <div style="padding: 8px 0px; margin-right:-50px;" class="input-group-prepend ">
-                    <i class="fas fa-search faz"></i>
-                </div>
-                <input type="text" class="form-control" placeholder="Buscar" id="txt-buscar" aria-label="Username" aria-describedby="basic-addon1">
-            </div>
+        <div class="modal-body">
+            Para crear una Pin, debe de crear un archivo csv 
             
-            <div>
-                <button type="button" class="btn btn-light letraNav " style="border-radius: 25px; height: 43px;"
-                >Inicio</button>
-            </div>
-
-            <div>
-                <button type="button" class="btn btn-light letraNav" style="border-radius: 25px; height: 43px;">
-                    Explorar</button>
-            </div>
-
-            <div>
-                <button type="button" class="btn btn-light letraNav" style="border-radius: 50px">
-                        <img src="img/perfil.jpg" width="30" height="30" style="border-radius: 100px;">
-                        aczayozabeth
-                </button>
-            </div>
-
-            <div>
-                <button type="button" class="btn btn-light letraNav rounded-circle">
-                    <i class="fas fa-comment-dots"></i>
-                </button>
-            </div>
-
-            <div>
-                <button type="button" class="btn btn-light letraNav rounded-circle">
-                    <i class="fas fa-bell"></i>
-                </button>
-            </div>
-
-            <div>
-                <button type="button" class="btn btn-light letraNav rounded-circle">
-                    <i class="fas fa-ellipsis-h"></i>
-                </button>
-            </div> 
-        </div> 
-    </nav>
-    
-    <main role="main" class="container" style=" margin: 5px; padding: 65px 0px 0px 0px; max-width: 100%;">
-        <hr>
-        <div class="row" style=" margin: 0px; padding: 0px 0px 0px 0px; max-width: 100%;">
-
-            <?php
-              //Forma facil
-              $archivo = fopen("data/".$_GET["carpeta"].".csv", "r");
-              $linea="";
-              while(($linea = fgets($archivo))){
-                $partes = explode(",",$linea);
-
-                echo'<div class="card columna " style="max-width: 260px; padding:8px;" id="'.$partes[0].'">
-                        <img class="card-img-top" src="img/'.$partes[1].'" id="idImagen" >
-                        <div class="card-body row">
-                            <h4 class="card-text letraNav">'.$partes[2].'</h4>
-                            <button type="button" class="btn btn-light letraNav rounded-circle" style=" padding:6px; position: relative;left:90px; width: 24px;height: 24px;" >
-                                <i class="fas fa-ellipsis-h" style="align-content: center"></i> </button>
-                            </button>
-                        </div>
-                    </div>';
-                }
-
-              fclose($archivo);
-
-            ?>
         </div>
-    </main>
-    
+        <div class="modal-footer">
+            <button type="button" class="btn btn-primary" id="btn-AgregarPin">Crear Pin</button>
+        </div>
+        </div>
+    </div>
+</div>
+
     <script src="js/jquery.min.js"></script>
-    <script src="js/controlador.js"></script>
+    <script src="js/controlador_inicio.js"></script>
+    <script src="jquery/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
     <script>
 
         var contador=0;
@@ -120,14 +180,16 @@
             
             $("#"+i).mouseenter(function(){
                 $("#"+i).css("background-color","#F2F2F2",); 
-                $("#"+i+" img").css("-webkit-filter","brightness(50%)","webkit-filter",
-                "brightness(50%)");
+                $("#"+i+" img").css("-webkit-filter","brightness(50%)","webkit-filter","brightness(50%)");
+               // $("#"+i).append("<div class='float-left' style='background: transparent'>Float</div><br>");
+
             });
 
             $("#"+i).mouseleave(function(){
                 $("#"+i).css("background-color","transparent");
                 $("#"+i+" img").css("-webkit-filter","brightness(100%)","webkit-filter",
                 "brightness(100%)");
+                //$("#"+i).html("<button type='button' class='btn btn-danger style='display:none'>Danger</button>");
             });
                 
         }
