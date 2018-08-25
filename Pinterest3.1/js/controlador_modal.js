e1ff3e6041b3533d0354ad514fe4b18e85cb905e
YozabethGarcia <31870247+YozabethGarcia@users.noreply.github.com>
2018-08-24 22:18:19 -0600

    AczaGarcia
    Arrastra o clic

--- Pinterest3.1/js/controlador_modal.js
+++ Pinterest3.1/js/controlador_modal.js
@@ -38,6 +38,49 @@ $(document).on('change','#fileElem',function(){
 	  $('#preview').html(img);
 	}
 });
+
+
+function handleFileSelect(evt) {
+
+	evt.stopPropagation();
+	evt.preventDefault();
+
+	var files = evt.dataTransfer.files; //FileList object.
+	console.log(files);
+
+	for (var i = 0, f; f = files[i]; i++) {
+
+		if (!f.type.match('image.*')) {
+			continue;
+		}
+		//lectura de objeto
+		var reader = new FileReader();
+
+		reader.onload = (function(theFile) {
+			return function(e) {
+
+			$("#preview").html(['<img id="Img-subir"style="width: 234px;" src="', e.target.result,
+									'" title="', escape(theFile.name), '"/>'].join(''));
+			};
+			
+		})(f);
+
+		// Lea en el archivo de imagen como una URL de datos.
+	   reader.readAsDataURL(f);
+	}
+}
+
+function handleDragOver(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+	evt.dataTransfer.dropEffect = 'copy'; // copia
+}
+
+// Configurar los dnd listeners.
+var dropZone = document.getElementById('drop_zone');
+dropZone.addEventListener('dragover', handleDragOver, false);
+dropZone.addEventListener('drop', handleFileSelect, false);
+
 /*
 $("#btn-AgregarPin").click(function () {
 
